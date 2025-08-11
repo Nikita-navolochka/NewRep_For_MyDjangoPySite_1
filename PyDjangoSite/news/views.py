61424a148211fb2f9361329724b6838c0d00c0a5
Nikita <g89871073306@gmail.com>
2025-08-11 22:11:16 +0500

    IT`S ALIVE!!! IT`S ALIVE!!!!

--- PyDjangoSite/news/views.py
+++ PyDjangoSite/news/views.py
@@ -1,7 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Articles
+from .forms import ArticlesForm
 # Create your views here.
 
-news = Articles.objects.all()
 def NewsHome(request):
+    news = Articles.objects.order_by('-date')
     return render(request, 'news/news.html', {'news':news})
+
+
+def CreateNews(request):
+    error=''
+    if request.method == 'POST':
+        form = ArticlesForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(' http://127.0.0.1:8000/')
+        else:
+            error = 'error'
+
+
+    form = ArticlesForm()
+
+    data = {
+        'form': form,
+        'error':error
+    }
+    return render(request, 'news/CreateNews.html', data)
+
